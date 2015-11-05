7f25b7dd77fa56c9d921d6997f855afeb4180aec
pmsardesai <pmsardesai@users.noreply.github.com>
2015-11-04 23:26:15 -0500

    Added Google Analytics. Also removed previous messy commits.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import GoogleAnalytics from 'react-google-analytics';
+import {Initializer} from 'react-google-analytics';
 import Header from './Header';
 import MenuBar from './MenuBar';
 
@@ -16,11 +18,8 @@ class App extends React.Component {
 		/*
 		* The state of the menu. It can be 'pinned' or 'unpinned'.
 		* Default: 'unpinned'
-		*
-		* The current page displayed.
-		* Default: 'home' -- the home page is displayed by default
 		*/
-		this.state = {menuState: 'unpinned', currentPage: 'home'};
+		this.state = {menuState: 'unpinned', isNewPage: true, pathname: '/'};
 	}
 
 	/*
@@ -29,20 +28,37 @@ class App extends React.Component {
 	onToggleMenuState(unpin) {
 		let state = unpin === true ? 'pinned' : this.state.menuState;
 		this.setState({
-			menuState: (state === 'unpinned') ? 'pinned' : 'unpinned'
+			menuState: (state === 'unpinned') ? 'pinned' : 'unpinned',
+			isNewPage: false
 		});
 
 		window.scrollTo(0, 0);
 	}
 
+	/*
+  	* Get new page details whenever the props changes
+  	*/
+	componentWillReceiveProps(nextProps) {
+		this.setState( {
+			isNewPage: true,
+			pathname: nextProps.location.pathname
+		});
+	}
+
 	render() {
 		const className = 'main-app ' + this.state.menuState;
+
+		// if user navigates to new page, send pageview to google analytics
+		const GA_TRACKING_CODE = 'UA-49795903-4';
+		GoogleAnalytics('create', GA_TRACKING_CODE);
+		this.state.isNewPage && GoogleAnalytics('send', 'pageview', this.state.pathname);
 
 		return (
 			<div className={className}>
 				<Header onClick={this.onToggleMenuState.bind(this)} />
 				{ this.props.children }
 				<MenuBar type='mobile' onItemClick={this.onToggleMenuState.bind(this)} />
+				<Initializer />
 			</div>
 		);
 	}
