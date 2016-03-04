91b592aa7fa4330abf56554454ddd548d84c92e0
pmsardesai <pmsardesai@users.noreply.github.com>
2016-03-04 17:35:26 -0500

    Added blaze caterers to highlights page

--- src/components/Page/Highlights.jsx
+++ src/components/Page/Highlights.jsx
@@ -26,6 +26,14 @@ class Concert extends React.Component {
 								There will be a scintillating dance performance by San Diego's very own renowned Film and Dance Choreographer, Jonathan Bosco. He graduated from Penn State University with Bachelors of Arts in Communications and Film Production. His main inspirational roots of his dance style are Breakin (or Bboying) followed by Popping/Boogaloo and Indian dances. He was a choreographer for Penn State Ghaamudyaz Garba-Raas 2004-2006 team, which won 7 back-to-back National 1st Place titles. He has worked with Tricia Miranda, Nakul Dev Mahajan, Wanted Ashiqz, Project Pulse, Doonya Dance Workout, Aatma Performing Arts, and many more. He has contributed his talent to the San Diego's Wild Animal Park & Safari Park, FOX’s So You Think You Can Dance, MTV’s America’s Best Dance Crew, Pulse On Tour, ZeeTV, StarPlus, SonyTV. He was chosen to compete in Bombay, India as the first ever Indo-Canadian-American male dancer by renowned Hindi Film Director, Farah Khan, and established Indian Choreographer, Vaibhavi Merchant and he reached Top 30 on Hrithik Roshan's reality show Star Plus JUST DANCE and was an assistant for the program. He is the extended member of India's infamous crew "I AM HIP HOP" directed by Ruel Dausan Varindani. Jonathan Bosco currently teaches Indian Hip Hop, Bollywood Freestyle and Chutney Cardio at The Naad Studios San Diego, Studio FX San Diego, Studio K Dance & Fitness, and with over 150 students.  
 							</div>
 						</div>
+
+						<div className="highlight">
+							<div className="name">Blaze brings his talent to Southern California all the way from Goa, India</div>
+							<div className="photo blaze"> </div>
+							<div className="description"> 
+								Coming from a third generation of catering business, Blaze started young, helping out the family with their catering and restaurant business. Blaze pursued his culinary career in Carnival Cruise Lines for over 12 years and then in Hyatt Hotels in Pittsburg, PA and La Jolla, San Diego and has been rewarded with many culinary awards. He is accomplished in all kinds of cuisines of around the world and specializes in Goan food. He has catered for Goan weddings and other events from Bakersfield to San Diego , CA. Blaze is happy to bring his culinary expertise to South California to tease your taste buds.
+							</div>
+						</div>
 					</div>
 				</div>
 				<Footer />
