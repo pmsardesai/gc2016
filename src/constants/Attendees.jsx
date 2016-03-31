387d3f98fe05ea5187052feeb0cb0cc4008b567b
pmsardesai <pmsardesai@users.noreply.github.com>
2016-03-30 22:26:36 -0400

    Updated attendees.

--- src/constants/Attendees.jsx
+++ src/constants/Attendees.jsx
@@ -7,171 +7,241 @@ module.exports =
 	{
 		lastName: "Araligidad",
 		firstNames: "Nilesh, Varsha Jagdale",
-		location: "Sunnyvale, CA"
+		location: "Sunnyvale, CA",
+		count: 2
+	},
+	{
+		lastName: "Borker",
+		firstNames: "Gaurav",
+		location: "Santa Clara, CA",
+		count: 1
 	},
 	{
 		lastName: "Bosco",
 		firstNames: "Jonathan",
-		location: "San Diego, CA"
+		location: "San Diego, CA",
+		count: 1
 	},
 	{
 		lastName: "Braganza",
 		firstNames: "Alfred, Nora",
-		location: "Los Angeles, CA"
+		location: "Los Angeles, CA",
+		count: 2
 	},
 	{
 		lastName: "Buyao",
 		firstNames: "Srinath, Vaishnavi",
-		location: "San Diego, CA"
+		location: "San Diego, CA",
+		count: 2
 	},
 	{
 		lastName: "Dalvi",
 		firstNames: "Arvind, Prabha",
-		location: "Edison, NJ"
+		location: "Edison, NJ",
+		count: 2
 	},
 	{
 		lastName: "Desai",
 		firstNames: "Sanjeet Hegde",
-		location: "Milpitas, CA"
+		location: "Milpitas, CA",		
+		count: 1
 	},
 	{
 		lastName: "Deshmukh",
 		firstNames: "Udayan, Sanjyot",
-		location: "Ottawa, ON, Canada"
+		location: "Ottawa, ON, Canada",
+		count: 2
+	},
+	{
+		lastName: "Fallary",
+		firstNames: "Shreya Naik",
+		location: "Minneapolis, MN",
+		count: 1
 	},
 	{
 		lastName: "Ferrao",
 		firstNames: "Eutimia",
-		location: "Santa Ana, CA"
+		location: "Santa Ana, CA",
+		count: 1
 	},
 	{
 		lastName: "Gaitonde",
 		firstNames: "Gajanan, Vidya",
-		location: "Uvalde, TX"
+		location: "Uvalde, TX",
+		count: 2
 	},
 	{
 		lastName: "Gaitonde",
 		firstNames: "Mangirish, Smita, Shivani",
-		location: "San Antonio, TX"
+		location: "San Antonio, TX",
+		count: 3
+	},
+	{
+		lastName: "Kamat",
+		firstNames: "Seema, Saanvi",
+		location: "Hernando, FL",
+		count: 2
 	},
 	{
 		lastName: "Kamat",
 		firstNames: "Subrat",
-		location: "Peoria, IL"
+		location: "Peoria, IL",
+		count: 1
 	},
 	{
 		lastName: "Kamatmhamai",
 		firstNames: "Vassant, Hema",
-		location: "Brookfield, CT"
+		location: "Brookfield, CT",
+		count: 2
 	},
 	{
 		lastName: "Keni",
 		firstNames: "Pradeep, Sadhana",
-		location: "Oak Brook, IL"
+		location: "Oak Brook, IL",
+		count: 2
 	},
 	{
 		lastName: "Kenkre",
 		firstNames: "Mahendra, Shama",
-		location: "Troy, MI"
+		location: "Troy, MI",
+		count: 2
 	},
 	{
 		lastName: "Keny",
 		firstNames: "Vasant",
-		location: "La Palma, CA"
+		location: "La Palma, CA",
+		count: 1
 	},
 	{
 		lastName: "Kundaikar",
 		firstNames: "Ganesh",
-		location: "Sunnyvale, CA"
+		location: "Sunnyvale, CA",
+		count: 1
 	},
 	{
 		lastName: "Lawande",
 		firstNames: "Shubhada, Sanjay, Niraj",
-		location: "Naperville, IL"
+		location: "Naperville, IL",
+		count: 3
 	},
 	{
 		lastName: "Lotlikar",
 		firstNames: "Sandeep, Madhavi, Piyusha, Narsimha Nimish",
-		location: "Elmont, NY"
+		location: "Elmont, NY",
+		count: 4
 	},
 	{
 		lastName: "Lotlikar",
 		firstNames: "Ulhas, Snehaprabha",
-		location: "Muttontown, NY"
+		location: "Muttontown, NY",
+		count: 2
+	},
+	{
+		lastName: "Laurenco",
+		firstNames: "Joseph",
+		location: "San Jose, CA",
+		count: 1
 	},
 	{
 		lastName: "McBride",
 		firstNames: "Agnes",
-		location: "Santa Ana, CA"
+		location: "Santa Ana, CA",
+		count: 1
 	},
 	{
 		lastName: "Nabar",
 		firstNames: "Nandan, Mohini Kamat, Neha",
-		location: "Fremont, CA"
+		location: "Fremont, CA",
+		count: 3
 	},
 	{
 		lastName: "Parkar",
 		firstNames: "Rohin, Vaibhavi, Ritika",
-		location: "San Jose, CA"
+		location: "San Jose, CA",
+		count: 3
 	},
 	{
 		lastName: "Raikar",
 		firstNames: "Ramesh, Shakuntala",
-		location: "Edison, NJ"
+		location: "Edison, NJ",
+		count: 2
 	},
 	{
 		lastName: "Rege",
 		firstNames: "Ratan, Rekha",
-		location: "Irvine, CA"
+		location: "Irvine, CA",
+		count: 2
+	},
+	{
+		lastName: "Rodrigues",
+		firstNames: "Savio",
+		location: "Redwood City, CA",
+		count: 1
 	},
 	{
 		lastName: "Rokade",
 		firstNames: "Prashant, Radhika Naik",
-		location: "Aliso Viejo, CA"
+		location: "Aliso Viejo, CA",
+		count: 2
 	},
 	{
 		lastName: "Sanvordeker",
 		firstNames: "Dilip, Meenal",
-		location: "Irvine, CA"
+		location: "Irvine, CA",
+		count: 2
 	},
 	{
 		lastName: "Sanwardeker",
 		firstNames: "Milind, Veena",
-		location: "North Canton, OH"
+		location: "North Canton, OH",
+		count: 2
 	},
 	{
 		lastName: "Sardesai",
 		firstNames: "Mahesh, Nita, Pooja",
-		location: "Mississauga, ON, Canada"
+		location: "Mississauga, ON, Canada",
+		count: 3
 	},
 	{
 		lastName: "Sardesai",
 		firstNames: "Raj, Smeeta, Nolan",
-		location: "Arcadia, CA"
+		location: "Arcadia, CA",
+		count: 3
 	},
 	{
 		lastName: "Shenoy",
 		firstNames: "Prakash",
-		location: "West Covina, CA"
+		location: "West Covina, CA",
+		count: 1
 	},
 	{
 		lastName: "Shirsat",
 		firstNames: "Maya",
-		location: "Curchorem, Goa, India"
+		location: "Curchorem, Goa, India",
+		count: 1
 	},
 	{
 		lastName: "Tarkar",
 		firstNames: "Akshay, Purnima",
-		location: "San Diego, CA"
+		location: "San Diego, CA",
+		count: 2
+	},
+	{
+		lastName: "Verekar",
+		firstNames: "Abhijit",
+		location: "NJ",
+		count: 1
 	},
 	{
 		lastName: "Verenkar",
 		firstNames: "Rudresh, Darshana, Aarav",
-		location: "Pembroke Pines, FL"
+		location: "Pembroke Pines, FL",
+		count: 3
 	},
 	{
 		lastName: "Virginkar",
 		firstNames: "Arun, Swati",
-		location: "Huntington Beach, CA"
+		location: "Huntington Beach, CA",
+		count: 2
 	}
 ];
